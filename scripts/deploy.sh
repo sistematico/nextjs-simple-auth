6a7f9e85cd35ba6f81dfb66193804bdfcd690237
Lucas Saliés Brum <sistematico@gmail.com>
2025-07-12 22:50:38 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,16 @@
 PATH=$PATH:/home/nginx/.bun/bin
 
 [ -f .env.production ] && cp .env.production /tmp/env.auth.lucasbrum
+
 git clean -fxd
+
 [ -f /tmp/env.auth.lucasbrum ] && cp /tmp/env.auth.lucasbrum .env.production
 sudo /usr/bin/systemctl stop auth.lucasbrum.dev.service
+
+bash scripts/db/drop.sh
+bash scripts/db/create.sh
+
 bun install
 bun run build
+
 sudo /usr/bin/systemctl start auth.lucasbrum.dev.service
