c4b42643b541d9f0a55c1f60aedc2a61115b43c3
Lucas Saliés Brum <sistematico@gmail.com>
2025-07-13 21:47:50 -0400

    Update

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # ansible-vault create ansible/roles/postgres/vars/vault.yml
+# echo "vault_password" > ansible/.vault_pass
 export ANSIBLE_VAULT_PASSWORD_FILE=./ansible/.vault_pass
 
 ROOT=$(dirname "$(readlink -f "$0")")
@@ -21,6 +22,6 @@ else
   ANSIBLE_PYTHON_INTERPRETER=auto_silent \
   ANSIBLE_CONFIG="${ROOT}/../ansible/ansible.cfg" \
   #ansible-playbook --vault-password-file ${ROOT}/../ansible/.vault_pass \
-  ansible-playbook --connection=local -e "ansible_port=2200" \
+  ansible-playbook -bK --connection=local -e "ansible_port=2200" \
     "${ROOT}/../ansible/main.yml" -i localhost,
 fi
