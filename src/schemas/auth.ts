a70ab941c10c7679fe31ea522edf67fe40eea62d
Lucas Saliés Brum <sistematico@gmail.com>
2025-07-27 16:04:40 -0400

    Update

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-// import type { Role } from "@/db/schema";
 
 export const signInSchema = z.object({
   email: z.email("Email inválido"),
@@ -10,8 +9,8 @@ export const signUpSchema = z.object({
   name: z.string().min(1, "Nome é obrigatório"),
   email: z.email("Email inválido"),
   password: z.string()
-    .min(6, "Senha deve ter pelo menos 6 caracteres")
-    .regex(/[A-Z]/, "Senha deve conter pelo menos uma letra maiúscula")
-    .regex(/[a-z]/, "Senha deve conter pelo menos uma letra minúscula")
-    .regex(/[0-9]/, "Senha deve conter pelo menos um número"),
+    .min(4, "Senha deve ter pelo menos 4 caracteres")
+    // .regex(/[A-Z]/, "Senha deve conter pelo menos uma letra maiúscula")
+    // .regex(/[a-z]/, "Senha deve conter pelo menos uma letra minúscula")
+    // .regex(/[0-9]/, "Senha deve conter pelo menos um número"),
 });
