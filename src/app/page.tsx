f905f50d3b3dd505699de397e40ca6b3dbd800e4
Lucas Saliés Brum <sistematico@gmail.com>
2025-10-28 04:59:07 -0400

    fix: update button border styles for consistency in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,14 +60,14 @@ export default function Home() {
                 </Link>
               )}
               <Link
-                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
                 href="/private"
               >
                 Private
               </Link>
               <button
               type="button"
-                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
                 onClick={async () => {
                   await logOut();
                 }}
@@ -91,7 +91,7 @@ export default function Home() {
                 Sign-In
               </Link>
               <Link
-                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
                 href="/signup"
               >
                 Sign-Up
