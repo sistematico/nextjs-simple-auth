c45f1bac9e475121d766785300413382eee0ac8d
Lucas Saliés Brum <sistematico@gmail.com>
2026-01-19 04:48:08 -0400

    feat: implement authentication pages and database schema with drizzle-orm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <Link href="/entrar">Entrar</Link>
+      <Link href="/cadastro">Cadastro</Link>
     </main>
   );
 }
