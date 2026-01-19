70972a429df3bcd74d2b4f2003c139bafdab02d0
Lucas Saliés Brum <sistematico@gmail.com>
2026-01-19 00:28:34 -0400

    Updates

---
+++ src/lib/session.ts
@@ -0,0 +1,71 @@
+import "server-only";
+
+import { cookies } from "next/headers";
+import { SignJWT, jwtVerify } from "jose";
+import { SessionPayload } from "@/schemas/auth";
+import { db } from "@/db";
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function encrypt(payload: SessionPayload) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+}
+
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed to verify session");
+  }
+}
+
+// export async function createSession(userId: string) {
+//   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+//   const session = await encrypt({ userId, expiresAt });
+//   const cookieStore = await cookies();
+
+//   cookieStore.set("session", session, {
+//     httpOnly: true,
+//     secure: true,
+//     expires: expiresAt,
+//     sameSite: "lax",
+//     path: "/",
+//   });
+// }
+
+export async function createSession(id: number) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  // 1. Create a session in the database
+  const data = await db
+    .insert(sessions)
+    .values({
+      userId: id,
+      expiresAt,
+    })
+    // Return the session ID
+    .returning({ id: sessions.id });
+
+  const sessionId = data[0].id;
+
+  // 2. Encrypt the session ID
+  const session = await encrypt({ sessionId, expiresAt });
+
+  // 3. Store the session in cookies for optimistic auth checks
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
